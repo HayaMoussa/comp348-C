0e01b65b43b94d9cf36ef72997fbc425cf2ffd4f
HayaMoussa <mohay@live.concordia.ca>
2023-10-16 23:19:58 -0400

    Table gen tokenized

--- tablegen.c
+++ tablegen.c
@@ -15,68 +15,60 @@ void show_array();
 int main(int argc, char **argv) {
 
     int choice = 0;      // 1 or 2 for show_menu1(). Automatic allocation so no free needed
-    int *columns = NULL; // Declare a pointer to store integers. Free needed after since dynamic allocation.
     int count = 0;       // Initialize a count to keep track of the number of integers read
-    char *inputColumns;   // todo: should this be char?
+    char *inputColumns = NULL;      // Declare a pointer to store the "raw" char gotten from scanf.
+    int *columns = NULL;            // Declare a pointer to store the casted integers used for manipulations
 
+    // TODO: remove this before submitting
     setbuf(stdout, 0);
+
     // Call method to show menu
     show_menu1(choice);
     
-	// User input 
+	// User input for main menu as an int
 	scanf("%d", &choice);
 
-    if (choice == 1)
-    {
+    if (choice == 1) {
         show_menu2();
-        
-            /******************************
-             * READING COLUMN LIST
-             * *************************/
 
-            columns = (int *)malloc(sizeof(int)); // NEED TO ALLOCATE END OF STRING!! +1
+        // Allocate memory for user input. 8 + 7 commas maximum.
+        inputColumns = (char *) malloc(sizeof(int) * 16);  // Allocate memory for user input
 
-            // scanf returns 1 if digit or comma found, last one will not have a comma and that is ok!
-            scanf("%s,", inputColumns); //4 in string is char 52 in ascii. noice!
+        // User input of the columns
+        scanf("%s", inputColumns);
 
-            // Searches for the first character which is not ',', todo: char *
-            char *token = strtok( inputColumns, ",");
-        
+        // Use strtok to return a pointer to the first CHARACTER of the first token.
+        char *token = strtok(inputColumns, ",");
+
+        // Loop until the last token
         while (token != NULL) {
-            
-                // Return an int value corresponding to the contents of str is returned. TODO: CAST INT HERE?
-                    inputColumns = (int *) strtol(token, NULL, 10);
-                
-                // Need realloc to re-size array if not first column. +1 for \0.
-                columns = (int *)realloc(columns, (count + 1) * sizeof(int));
-                
-                // Assign the value entered to the correct index in the array
-                columns[count] = inputColumns;
-                
-                // Increment for next loop
-                count++;
-                
-                // NULL to continue tokenizing the same string from where it left off in the previous call.
-                // https://en.cppreference.com/w/c/string/byte/strtok
-                token = strtok(NULL, ","); 
-            }
-         
-        // Deallocation for first malloc
-        free(columns);	
-        
-    } 
-    
-    else if (choice == 2) 
+            // Convert to a long int the token found by strtok
+            int columnValue = (int) strtol(token, NULL, 10);
+
+            // Increase the size of the array with the number of digits entered by the user
+            columns = (int *) realloc(columns, (count + 1) * sizeof(int));
+
+            // Set the digit entered at the index
+            columns[count] = columnValue;
+
+            // Increment to keep track of the indexes for future tokens
+            count++;
+
+            // Repeat loop
+            token = strtok(NULL, ",");
+        }
+
+        // Deallocation needed due to malloc (dynamic)
+        free(inputColumns);  // Deallocate user input memory
+    }
+    else if (choice == 2)
     {
         printf ("Goodbye and thanks for using TableGen\n");
     }
-    
+
     // Terminate program successfully
     return 0;
 }
-
-
-
 
 
 
@@ -112,3 +104,50 @@ void show_menu2(int choice) {
     printf("Enter column list (comma separated, no spaces):");
 }
 
+
+
+/*   if (choice == 1)
+    {
+        // Show column choices
+        show_menu2();
+
+        *//******************************
+         * READING COLUMN LIST
+         * *************************//*
+
+        columns = (int *)malloc(sizeof(int)); // NEED TO ALLOCATE END OF STRING!! +1
+        inputColumns = (char *)malloc(sizeof(int) * 16); // Allocate memory for user input 15
+
+
+        // scanf returns 1 if digit or comma found, last one will not have a comma and that is ok!
+        scanf("%s,", inputColumns); //4 in string is char 52 in ascii. noice!
+
+        // Searches for the first character which is not ',', todo: char *
+        char *token = strtok( inputColumns, ",");
+
+
+        while (token != NULL) {
+
+            // Return an int value corresponding to the contents of str is returned. TODO: CAST INT HERE?
+            //inputColumns = (int) strtol(token, NULL, 10);
+            int columnValue = (int)strtol(token, NULL, 10);
+
+
+            // Need realloc to re-size array if not first column. +1 for \0.
+            columns = (int *)realloc(columns, (count + 1) * sizeof(int));
+
+            // Assign the value entered to the correct index in the array
+            columns[count] = inputColumns;
+
+            // Increment for next loop
+            count++;
+
+            // NULL to continue tokenizing the same string from where it left off in the previous call.
+            // https://en.cppreference.com/w/c/string/byte/strtok
+            token = strtok(NULL, ",");
+        }
+
+        // Deallocation for first malloc
+        free(columns);
+
+    }*/
