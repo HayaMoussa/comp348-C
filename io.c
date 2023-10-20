21c52d70e2951a0e6d8530cb50724c61231df7a4
HayaMoussa <mohay@live.concordia.ca>
2023-10-20 12:33:11 -0400

    Optimized header writing, one for loop removed

--- io.c
+++ io.c
@@ -49,81 +49,96 @@ void write_file(char *filename, int *columns, int rowCount) {
     // Looping through the "array" of columns saved in select_columns to generate header
     int columnIndex;       // For loop of column values
     int max = sizeof(columns) / sizeof(int);
-    for (columnIndex = 0; columnIndex <= max; ++columnIndex) // size of returns the size of THE pointer so need division
-    {
-        switch(columns[columnIndex])
-        {
-            case 1:
-                fprintf(file,"User ID");
-                break;
-            case 2:
-                fprintf(file,"First Name");
-                break;
-            case 3:
-                fprintf(file,"Last Name");
-                break;
-            case 4:
-                fprintf(file,"Country");
-                break;
-            case 5:
-                fprintf(file,"Phone Number");
-                break;
-            case 6:
-                fprintf(file,"Email Address");
-                break;
-            case 7:
-                fprintf(file,"SIN");
-                break;
-            case 8:
-                fprintf(file,"Password");
-                break;
-        }
-
-        // If last one, do not print a comma
-        if (columnIndex != max)
-        {
-            fprintf(file,",");
-        }
-
-    }
-    fprintf(file, "\n");
-
-    /****************************************
-     * WRITING THE GENERATED DATA TO FILE
-     ****************************************/
-
-    // TODO: Write the actual line of data
-    //fprintf(file, "%s,%s,%s\n", columns[0], columns[1], columns[2]);
-
-    // TODO: Maybe the loops need to be switched.
-    // Looping through the "array" of columns saved in select_columns to generate data
 
     // TODO: Teacher used fput? I used fprintf for it's formatting options.
     int rowWritten;
-    for (rowWritten = 0; rowWritten < rowCount; ++rowWritten)
+    for (rowWritten = -1; rowWritten < rowCount; ++rowWritten) // starts at -1 for row header.
     {
+        // Looping through the "array" of columns saved in select_columns to generate data
         for (columnIndex = 0; columnIndex <= max; ++columnIndex) // size of returns the size of THE pointer so need division
         {
             switch (columns[columnIndex]) {
                 case 1: // User ID
-                    fprintf(file, "%d", arrayID[rowWritten]);
+                    // This writes the header
+                    if (rowWritten==-1)
+                    {
+                        fprintf(file,"User ID");
+                    }
+                    // This writes the data
+                    else
+                    {
+                        fprintf(file, "%d", arrayID[rowWritten]);
+                    }
                     break;
                 case 2: // First Name
+                    if (rowWritten==-1)
+                    {
+                        fprintf(file,"First Name");
+                    }
+                    else
+                    {
+                        fprintf(file, "NOPE");
+                    }
                     break;
                 case 3: // Last Name
+                    if (rowWritten==-1)
+                    {
+                        fprintf(file,"Last Name");
+                    }
+                    else
+                    {
+                        fprintf(file, "NOPE");
+                    }
                     break;
                 case 4: // Country
+                    if (rowWritten==-1)
+                    {
+                        fprintf(file,"Country");
+                    }
+                    else
+                    {
+                        fprintf(file, "NOPE");
+                    }
                     break;
                 case 5: // Phone Number
-                    fprintf(file, "Phone Number");
+                    if (rowWritten==-1)
+                    {
+                        fprintf(file,"Phone Number");
+                    }
+                    else
+                    {
+                        fprintf(file, "NOPE");
+                    }
                     break;
                 case 6: // Email Address
+                    if (rowWritten==-1)
+                    {
+                        fprintf(file,"Email Address");
+                    }
+                    else
+                    {
+                        fprintf(file, "NOPE");
+                    }
                     break;
                 case 7: // SIN
-                    fprintf(file, "SIN");
+                    if (rowWritten==-1)
+                    {
+                        fprintf(file,"SIN");
+                    }
+                    else
+                    {
+                        fprintf(file, "NOPE");
+                    }
                     break;
                 case 8: // Password
-                    break;
+                    if (rowWritten==-1)
+                    {
+                        fprintf(file,"Password");
+                    }
+                    else
+                    {
+                        fprintf(file, "NOPE");
+                    }
             }
 
             // If last one, do not print a comma
@@ -147,3 +162,7 @@ void write_file(char *filename, int *columns, int rowCount) {
  * - Code the generate for the first 4
  * - Commit
  * */
+
+
+// Write the actual line of data
+//fprintf(file, "%s,%s,%s\n", columns[0], columns[1], columns[2]);
